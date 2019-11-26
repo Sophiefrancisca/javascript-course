aca91051429ce80659a2256e6394dd2c9d8bd762
Sophie <sfvanmilaan@gmail.com>
2019-11-26 21:44:55 +0100

    Section 2.10

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,3 +1,7 @@
+/*****************************
+*Variables and data types
+*/
+/*
 var firstName = 'Sophie';
 console.log(firstName);
 
@@ -12,3 +16,73 @@ console.log(job);
 
 job = 'Teacher';
 console.log(job);
+
+//Variable naming rules
+var _3years = 3;
+var johnMark = 'John and MArk';
+var if = 23;
+*/
+
+
+/*****************************
+*Variable and type coercion
+*/
+
+/*
+var firstName = 'Sophie';
+var age = 22;
+
+// Type coercion
+console.log(firstName + ' ' + age);
+
+var ob, isMarried;
+job = 'teacher';
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' years old ' + job + '. Is she married? ' + isMarried);
+
+// Variable mutation
+age = 'twenty two';
+job = 'driver';
+
+alert(firstName + ' is a ' + age + ' years old ' + job + '. Is she married? ' + isMarried);
+
+var lastName = prompt ('What is her last name?');
+console.log(firstName + ' ' + lastName);
+*/
+
+
+
+
+/***************************************8
+ * Basic operators
+ */
+
+var now, yearSophie, yearRicardo;
+ageSophie = 22;
+ageRicardo = 25;
+
+// Math operators
+now = 2019;
+yearSophie = now - 22;
+yearRicardo = now - 25;
+
+console.log(yearSophie);
+
+console.log(now + 2);
+console.log(now * 2);
+console.log(now / 10);
+
+
+// Logical operators
+var sophieOlder = ageSophie < ageRicardo;
+console.log(sophieOlder);
+
+
+// Typeof operator
+
+console.log(typeof sophieOlder);
+console.log(typeof ageSophie);
+console.log(typeof 'Rircardo is older than Sophie');
+var x;
+console.log(typeof x);
