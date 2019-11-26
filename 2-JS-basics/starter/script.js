99e3ffce72498231063dd730f1d11c39674c4432
Sophie <sfvanmilaan@gmail.com>
2019-11-26 20:21:06 +0100

    Section 2.8

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,14 @@
+var firstName = 'Sophie';
+console.log(firstName);
+
+var lastName = 'van Milaan';
+var age = 22;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
