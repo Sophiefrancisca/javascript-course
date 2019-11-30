ef61a3b0601ac06b8413eaa2f0be576a469da506
Sophie <sfvanmilaan@gmail.com>
2019-11-30 11:08:10 +0100

    section 3

--- 3-how-JS-works/starter/script.js
+++ 3-how-JS-works/starter/script.js
@@ -1,29 +1,45 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
+/*
+
+//functions
+calculateAge(1965);
 
 
+function calculateAge(year) {
+    console.log(2016 - year);
+}
 
 
+//retirement(1965); // doesn't work
 
+var retirement = function(year) {
+    console.log(65 - (2016 - year));
+}
 
+// Variable
 
+//console.log(age); // undefined
+var age = 23;
+console.log(age);
 
+function foo() {
+    console.log(age);
+    var age = 65;
+    console.log(age);
+}
+foo();
+console.log(age);
 
-
-
-
-
-
-
-
+*/
 
 ///////////////////////////////////////
 // Lecture: Scoping
 
 
 // First scoping example
-
 /*
+
 var a = 'Hello!';
 first();
 
@@ -36,6 +52,7 @@ function first() {
         console.log(a + b + c);
     }
 }
+
 */
 
 
@@ -52,14 +69,16 @@ function first() {
 
     function second() {
         var c = 'Hey!';
-        third()
+        third();
     }
 }
 
+// THIS IS NOT POSIBLE
 function third() {
     var d = 'John';
     console.log(a + b + c + d);
 }
+
 */
 
 
@@ -67,11 +86,40 @@ function third() {
 ///////////////////////////////////////
 // Lecture: The this keyword
 
+//console.log(this);
+/*
+calculateAge(1985);
 
+function calculateAge(year) {
+    console.log(2016 - year);
+    console.log(this); //Window
+}
+*/
 
+var john = {
+    name: 'john',
+    yearOfBirth: 1990,
+    calculateAge: function() {
+        console.log(this);
+        console.log(2016 - this.yearOfBirth);
 
+        /*
+        function innerFunction(){
+            console.log(this); // Window
+        }
+        innerFunction();
+        */
+    }
+}
 
+john.calculateAge();
 
+var mike =  {
+    name: 'Mike',
+    yearOfBirth: 1984,
 
+};
 
+mike.calculateAge = john.calculateAge;
+mike.calculateAge();
 
